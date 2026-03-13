1a468be234e20dbf38783ea877f0df3401bb6d55
jmartelo1 <jmartelo@infodoc.com.co>
2026-03-13 10:02:36 -0500

    chore: configure deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/f_flow_builder',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
